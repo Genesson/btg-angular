80bb1ab417a1dd5613a4698b6bb4854ee9419df6
Genesson Andrei Sauer <genesson.sauer@mjv.net>
2022-05-30 20:06:05 -0300

    chore: ajust imports app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
@@ -7,6 +8,7 @@ import { NgxsModule } from '@ngxs/store';
 import { environment } from './../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     NgxsModule.forRoot([], { developmentMode: !environment.production }),
     NgxsLoggerPluginModule.forRoot(),
-    NgxsReduxDevtoolsPluginModule.forRoot()
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    MainModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
