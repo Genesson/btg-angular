04b28614c830856ed802299055f7ab1995b68ff4
Genesson Andrei Sauer <genesson.sauer@mjv.net>
2022-05-30 20:05:03 -0300

    feat(header): create footer module

---
+++ src/app/shared/components/footer/footer.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FooterComponent } from './containers/footer/footer.component';
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [FooterComponent]
+})
+export class FooterModule { }
