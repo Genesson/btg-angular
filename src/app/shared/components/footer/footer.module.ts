4a7bbbcbf6f00fd15e47a334c1ac7193ea08f69f
Genesson Andrei Sauer <genesson.sauer@mjv.net>
2022-05-31 21:14:28 -0300

    feat(footer): create footer component

--- src/app/shared/components/footer/footer.module.ts
+++ src/app/shared/components/footer/footer.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SocialComponent } from './components/social/social.component';
 import { FooterComponent } from './containers/footer/footer.component';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, SocialComponent],
   imports: [
     CommonModule
   ],
