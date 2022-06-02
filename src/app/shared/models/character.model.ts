3ec96f6a6228a91c77dd15413b85f47b9b6601a4
Genesson Andrei Sauer <genesson.sauer@mjv.net>
2022-06-02 09:37:51 -0300

    fix(models): ajust character model

--- src/app/shared/models/character.model.ts
+++ src/app/shared/models/character.model.ts
@@ -1,11 +1,36 @@
-import { CharacterDataModel } from './character-data.model';
+export class CharacterUrlModel {
+  type: string;
+  url: string;
+}
+
+export class CharacterThumbnailModel {
+  path: string;
+  extension: string;
+}
+
+export class CharacterItemModel {
+  resourceURI: string;
+  name: string;
+  type?: string;
+}
+
+export class CharacterAssetsModel {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: Array<CharacterItemModel>;
+}
 
 export class CharacterModel {
-  code: number;
-  status: string;
-  copyright: string;
-  attributionText: string;
-  attributionHTML: string;
-  data: CharacterDataModel;
-  etag: string
+  id: number;
+  name: string;
+  description: string;
+  modified: Date;
+  resourceURI: string;
+  urls: Array<CharacterUrlModel>;
+  thumbnail: CharacterThumbnailModel;
+  comics: CharacterAssetsModel;
+  stories: CharacterAssetsModel;
+  events: CharacterAssetsModel;
+  series: CharacterAssetsModel;
 }
